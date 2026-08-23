bb359ffa72f7b892e420d1c96792cf04c178f045
CalGuo <calvinguogtch@gmail.com>
2026-08-23 02:00:54 -0400

    Add experimental serverActions with bodySizeLimit

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,21 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseHost = supabaseUrl ? new URL(supabaseUrl).host : "**";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: supabaseHost,
         pathname: "/storage/v1/object/public/menu-assets/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
       },
     ],
   },
